8771aecfd59bb5093553b27bfe15b6d50b4ee9ce
Andreas Plesner <aplesner@ee-tik-vm057.ethz.ch>
2025-04-10 13:14:39 +0200

    Syncing

--- scripts/update_database.sh
+++ scripts/update_database.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+eval "$(ssh-agent -s)"
+ssh-add /local/home/aplesner/.ssh/id_rsa
+
+cd /local/home/aplesner/cluster-usage
+
 ./scripts/fetch_logs.sh
 ./scripts/import_logs.sh
+
+kill $SSH_AGENT_PID
