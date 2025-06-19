792383e1c37db2ed7de74b299125a25a707bd722
aplesner <27522600+aplesner@users.noreply.github.com>
2025-06-19 18:21:15 +0200

    update script

--- scripts/create-cluster-usage-logs.sh
+++ scripts/create-cluster-usage-logs.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Source variables
+eval "$(ssh-agent -s)"
+ssh-add /local/home/aplesner/.ssh/id_rsa
+
+cd /var/www/cluster-usage-dashboard
+
 source scripts/variables.sh
 
 # ssh into the server, cd to ~/bin, and run `./slurm-script.sh --last-hours 2`
-ssh "$USER@$SERVER" << EOF
+ssh "$USER@$SERVER" << SCRIPT
+    cd ~/bin
+    ./slurm-script.sh --last-hours 2
+SCRIPT
 
-cd ~/bin
-./slurm-script.sh --last-hours 2
+kill $SSH_AGENT_PID
