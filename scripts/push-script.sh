0fb11598c64756ea5348d56cea1ef31f02a5b1e7
ijorl <mathys.joel@gmail.com>
2025-07-17 14:37:53 +0200

    try to fix scripts wo andreas

--- scripts/push-script.sh
+++ scripts/push-script.sh
@@ -2,11 +2,11 @@
 
 source scripts/variables.sh
 
-# Push slurm-script.sh to the server at `~/bin/slurm-script.sh`
-rsync -avz --progress scripts/slurm-script.sh "$USER@$SERVER:~/bin/slurm-script.sh"
-
 eval "$(ssh-agent -s)"
 ssh-add /local/home/aplesner/.ssh/id_rsa
+
+# Push slurm-script.sh to the server at `~/bin/slurm-script.sh`
+rsync -avz --progress scripts/slurm-script.sh "$USER@$SERVER:~/bin/slurm-script.sh"
 
 # ssh into the server and make the script executable
 ssh "$USER@$SERVER" << SCRIPT
