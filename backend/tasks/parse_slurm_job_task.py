f863845663f300a552c5fda8b46c4991d16420f4
ijorl <mathys.joel@gmail.com>
2025-07-25 14:37:48 +0200

    change directory for slurm log

--- backend/tasks/parse_slurm_job_task.py
+++ backend/tasks/parse_slurm_job_task.py
@@ -1,14 +1,16 @@
 from backend.parsers.slurm_parser import parse_slurm_log, store_slurm_jobs
 from backend.tasks.calendar_tasks import CALENDAR_LOGS_DIR
-from config import DB_PATH
+from config import DB_PATH, SLURM_DIRECTORY
 import os
 import logging
+
+
 
 logger = logging.getLogger(__name__)
 
 def parse_and_store_slurm_log():
     """Scheduled task to parse the Slurm log and store jobs in the database."""
-    slurm_log_file = os.path.join(CALENDAR_LOGS_DIR, 'slurm', 'slurm.log')
+    slurm_log_file = SLURM_DIRECTORY
     if not os.path.exists(slurm_log_file):
         logger.warning(f"Slurm log file not found: {slurm_log_file}")
         return {"status": "skipped", "reason": "Slurm log file not found"}
